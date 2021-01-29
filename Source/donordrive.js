687008e0ee005e9edcd37e052b54232613345ecf
Kevin-Santoyo <74026108+Kevin-Santoyo@users.noreply.github.com>
2021-01-28 19:03:52 -0800

    Added second function for donation list

--- Source/donordrive.js
+++ Source/donordrive.js
@@ -2,16 +2,19 @@
 
 
 
-localStorage.setItem("etag", "");
+localStorage.setItem("etagTotal", "");
+localStorage.setItem("etagDonation", "");
+var participantLink = 'https://extralife.donordrive.com/api/participants/448764';
+var donationLink = 'https://extralife.donordrive.com/api/participants/448764/donations?limit=1';
 
 
 function getDonationInfo() {
 	
 	const myHeaders = new Headers({
-		'If-none-match': localStorage.getItem('etag')
+		'If-none-match': localStorage.getItem('etagTotal')
 	});
 	
-	const requestInfo = new Request('https://extralife.donordrive.com/api/participants/448764', {
+	const requestInfo = new Request(participantLink, {
 		method: 'GET',
 		headers: myHeaders,
 		mode: 'cors',
@@ -30,12 +33,53 @@ function getDonationInfo() {
 					goalTarget.innerHTML = currentDonations + " / " + fundraiserGoal;
 				});
 				var etag = response.headers.get('etag');
-				localStorage.setItem("etag", etag);
+				localStorage.setItem("etagTotal", etag);				
+				console.log('Local storage updated');
+				getDonationList();
 			}
 		}).catch(function(err) {
 				console.error(` Err: ${err}`);
 			});
 	
+}
+
+function getDonationList() {
+	
+	const myHeaders = new Headers({
+		'If-none-match': localStorage.getItem('etagDonation')
+	});
+	
+	const requestInfo = new Request(donationLink, {
+		method: 'GET',
+		headers: myHeaders,
+		mode: 'cors',
+		cache: 'default',
+	});
+	
+	fetch(requestInfo)
+		.then(function(response) {
+			if (response.status == 304) {
+				console.log("Data unchanged");
+			} else {
+				response.json().then(data => {
+					var data = data[0];
+					var donorGroup = document.getElementById("donation");
+					var donorName = data.displayName;
+					var donorAmount = data.amount;
+					if (data.message) {
+						var donorMessage = data.message;
+						donorGroup.children[2].innerHTML = donorMessage;
+					}
+					donorGroup.children[0].innerHTML = donorName;
+					donorGroup.children[1].innerHTML = donorAmount;					
+				});
+				var etag = response.headers.get('etag');
+				localStorage.setItem("etagDonation", etag);
+				console.log('Local storage updated');
+			}
+		}).catch(function(err) {
+				console.error(` Err: ${err}`);
+			});
 }
 
 
